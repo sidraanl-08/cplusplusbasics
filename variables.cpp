adeb528622deb9bf0bc9f66ad327d8f0f8cfc6b7
SidraNl <sidraa.nl08@gmail.com>
2025-12-17 15:47:13 +0100

    made c++ variables

---
+++ variables.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+//integer
+int main(){
+    int age = 20;
+    cout << "My age is:"<< age<<endl;
+
+//float
+    float num = 334.58;
+    cout << "This is a float:" << num <<endl;
+
+//character
+
+    char grade = 'A';
+    cout <<"Grade:"<< grade << endl;
+
+//boolean
+
+    bool isStudent = true;
+    cout <<"Is Student:"<< isStudent << endl;
+
+//string
+
+    string name = "Sidra";
+    cout <<"Name:"<< name << endl;
+
+    return 0;
+}
+
+
+
