2797260d0e84ff7e71dfe3ae9520e64c154bcf65
SidraNl <sidraa.nl08@gmail.com>
2025-12-17 14:53:45 +0100

    made basic cpp structure

---
+++ basicstructure.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //header file that helps with input/output
+
+//main is the entry point where program initiates 
+//while int will specify that main returns an integer to OS 
+//standard is the namespace that contains standard c++ functions like cout and cin
+//return 0 is the successful termination of program which sends 0 value to OS 
+
+int main() {
+    std::cout<<"Hello World";
+    return 0;
+}
