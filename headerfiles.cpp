c5e6ab3e252afd3a9f29b25f730130e6d22da169
SidraNl <sidraa.nl08@gmail.com>
2025-12-18 11:42:28 +0100

    header files 📁

---
+++ headerfiles.cpp
@@ -0,0 +1,12 @@
+//there are 2 types of header files:
+//system header files: they come with compiler
+#include<iostream>
+//user-defined header files: they are written by programmer
+#include "this.sidra"
+
+using namespace std;
+
+int main(){
+    cout<<"This is program :)";
+    return 0;
+}
