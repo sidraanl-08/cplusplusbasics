951bdf5401b6fb9546a8632bb61de302db430222
SidraNl <sidraa.nl08@gmail.com>
2025-12-18 10:27:03 +0100

    updated comments

--- basicstructure.cpp
+++ basicstructure.cpp
@@ -1,7 +1,7 @@
-#include <iostream> //file that handles input/output
+#include <iostream> //It includes the header file that provides input/output streams (std::cout, std::cin),
 
 //main is the entry point where program initiates 
-//while int will specify that main returns an integer to OS 
+//int specifies the return type of main(), which is sent to the OS. 
 //standard is the namespace that contains standard c++ functions like cout and cin
 //return 0 is the successful termination of program which sends 0 value to OS 
 
