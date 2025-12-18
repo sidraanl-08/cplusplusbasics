b7914966008ee96f6b68e31699873339e56fde0f
SidraNl <sidraa.nl08@gmail.com>
2025-12-18 10:39:37 +0100

    updated variables of c++ 🤓

--- variables.cpp
+++ variables.cpp
@@ -7,9 +7,15 @@ int main(){
     int age = 20;
     cout << "My age is:"<< age<<endl;
 
-//float
+//float (decimal numbers of low precision)
+
     float num = 334.58;
     cout << "This is a float:" << num <<endl;
+
+//double (decimal numbers of high precision)
+
+    double num = 123.578903938;
+    cout << "This is a double:" << num <<endl;
 
 //character
 
@@ -31,3 +37,7 @@ int main(){
 
 
 ///understanding variable scope
+//it is the region in the code where existence of a variable is valid
+//local variables: declared inside braces of any function, can be accessed only from there
+//global variables: declared outside function and can be accessed only from anywhere
+//global and local variables can have same name in C++
